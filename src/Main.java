df270ecded78970ebbfae1a1c092d0b3d015b13f
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-02-15 20:27:59 -0700

    First commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Greetings Earthlings.");
         System.out.println("I am Spock.");
+        System.out.println("Logic has nothing to do with this.");
     }
 }
